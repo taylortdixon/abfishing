8e0fe2dc23228f35ee00ba6544c1f78d32190bab
Taylor Dixon <taylor.t.dixon@gmail.com>
2021-04-15 21:11:25 -0600

    Adding main menu nav

---
+++ src/components/main-menu-nav/main-menu-nav.tsx
@@ -0,0 +1,9 @@
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+
+export const MainMenuNav = () => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6">Alberta Fishing Regulations</Typography>
+    </Toolbar>
+  </AppBar>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MainMenuNav } from "./components/main-menu-nav/main-menu-nav";
 
 ReactDOM.render(
   <React.StrictMode>
+    <MainMenuNav />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
