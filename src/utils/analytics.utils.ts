0bf73c4f17aa4576913ab3d0075009961b8881d2
Taylor Dixon <taylor.t.dixon@gmail.com>
2021-05-22 07:46:03 -0600

    Fixing waterbody open event

--- src/utils/analytics.utils.ts
+++ src/utils/analytics.utils.ts
@@ -1,5 +1,8 @@
 export const trackWaterbodyOpen = (waterbodyName: string) => {
-  if ("ga" in window && (window as any).ga.loaded) {
-    (window as any).ga("send", "event", "waterbody", "open", waterbodyName);
+  if ("dataLayer" in window && (window as any).dataLayer.length > 0) {
+    (window as any).gtag("event", "open", {
+      event_category: "waterbody",
+      event_label: waterbodyName,
+    });
   }
 };
