07dc05266ba452c10f7015773cb5de70974c927f
Taylor Dixon <taylor.t.dixon@gmail.com>
2023-05-27 08:59:53 -0600

    Fixing default load state

--- src/components/all-providers/all-providers.tsx
+++ src/components/all-providers/all-providers.tsx
@@ -7,7 +7,7 @@ import WarningModal from "../warning-modal/warning-modal";
 export const AllProviders: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     setTimeout(() => setShowModal(true), 2000);
