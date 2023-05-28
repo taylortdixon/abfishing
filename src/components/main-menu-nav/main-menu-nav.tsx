055ec400968e4e55761d751418ac1d6a309dabb2
Taylor Dixon <taylor.t.dixon@gmail.com>
2023-05-28 07:39:49 -0600

    Optimizing main menu logo

--- src/components/main-menu-nav/main-menu-nav.tsx
+++ src/components/main-menu-nav/main-menu-nav.tsx
@@ -4,7 +4,13 @@ import styles from "./main-menu-nav.module.css";
 export const MainMenuNav = () => (
   <AppBar position="static">
     <Toolbar>
-      <img className={styles.main_menu_nav__logo} src="/icon.png" alt="" />
+      <img
+        className={styles.main_menu_nav__logo}
+        src="/icon.png"
+        alt="AB Fishing Logo"
+        width={50}
+        height={32}
+      />
       <Typography variant="h6">AB Fishing</Typography>
     </Toolbar>
   </AppBar>
